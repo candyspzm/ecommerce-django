8413f2406c9297d39d404e7cd3b667c328254663
Serhii <possession85@gmail.com>
2024-08-19 22:00:50 +0300

    views.py funcs added

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+
+def product_list(request):
+    pass
+
+
+def product_detail(request, id, slug):
+    pass
