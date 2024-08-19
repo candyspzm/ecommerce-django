f11cfa77bee4cd2c43ce25225c1b9a11ec6e0be8
Serhii <possession85@gmail.com>
2024-08-19 21:55:29 +0300

    admin classes registered models classes added

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категорія'
+        verbose_name_plural = 'Категорiї'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250)
+    image = models.ImageField(blank=True, upload_to='products/%Y/%m/%d')
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукти'
+
+    def __str__(self):
+        return self.name
